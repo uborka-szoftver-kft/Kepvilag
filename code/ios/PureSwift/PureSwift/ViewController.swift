3bccd2bd35691073c53d977a74d7491d35cf1210
Laurent <laurent@his.computer>
2019-01-11 20:51:44 +0100

    Sample of programmatic View creation in PureSwift project.

--- code/ios/PureSwift/PureSwift/ViewController.swift
+++ code/ios/PureSwift/PureSwift/ViewController.swift
@@ -1,6 +1,8 @@
 //
 //  ViewController.swift
 //  PureSwift
+//  Demonstrates programmatic View creation and layout
+//  https://softauthor.com/ios-ui-design-programmatically-xcode-9-swift-4/
 //
 //  Created by Laurent on 2019. 01. 11..
 //  Copyright © 2019. Laurent. All rights reserved.
@@ -10,9 +12,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let loginContentView:UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let unameTxtField:UITextField = {
+        let txtField = UITextField()
+        txtField.backgroundColor = .white
+        txtField.borderStyle = .roundedRect
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        return txtField
+    }()
+    
+    private let pwordTxtField:UITextField = {
+        let txtField = UITextField()
+        txtField.borderStyle = .roundedRect
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        return txtField
+    }()
+    
+    let btnLogin:UIButton = {
+        let btn = UIButton(type:.system)
+        btn.backgroundColor = .blue
+        btn.setTitle("Login", for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 5
+        btn.clipsToBounds = true
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    private func setUpAutoLayout() {
+        
+        // httpss://developer.apple.com/library/content/documentation/UserExperience/Conceptual/AutolayoutPG/ProgrammaticallyCreatingConstraints.html
+        loginContentView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        loginContentView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3).isActive = true
+        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        unameTxtField.topAnchor.constraint(equalTo:loginContentView.topAnchor, constant:40).isActive = true
+        unameTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        unameTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        unameTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+
+        pwordTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        pwordTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        pwordTxtField.topAnchor.constraint(equalTo:unameTxtField.bottomAnchor, constant:20).isActive = true
+        pwordTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+
+        btnLogin.topAnchor.constraint(equalTo:pwordTxtField.bottomAnchor, constant:20).isActive = true
+        btnLogin.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        btnLogin.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        btnLogin.heightAnchor.constraint(equalToConstant:50).isActive = true
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .darkGray
+        view.addSubview(loginContentView)
+        loginContentView.addSubview(unameTxtField)
+        loginContentView.addSubview(pwordTxtField)
+        loginContentView.addSubview(btnLogin)
+        
+        setUpAutoLayout()
     }
 
 
