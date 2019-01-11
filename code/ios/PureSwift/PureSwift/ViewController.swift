e6023fd477bdb616bb56fd8fed9795944e12e5f8
Laurent <laurent@his.computer>
2019-01-11 15:37:22 +0100

    PureSwift project for studying Xcode behavior.

---
+++ code/ios/PureSwift/PureSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PureSwift
+//
+//  Created by Laurent on 2019. 01. 11..
+//  Copyright © 2019. Laurent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
