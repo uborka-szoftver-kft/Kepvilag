e515f7b1b7d200170714ec8920321762cc65b31f
Laurent <laurent@his.computer>
2019-01-22 17:46:28 +0100

    Generating 'hello1' project using libGDX Project Setup.'

---
+++ code/hello1/android/src/io/github/uborkaszoftverkft/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.uborkaszoftverkft;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.uborkaszoftverkft.Hello1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Hello1(), config);
+	}
+}
---
+++ code/hello1/desktop/src/io/github/uborkaszoftverkft/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.uborkaszoftverkft.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.uborkaszoftverkft.Hello1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Hello1(), config);
+	}
+}
