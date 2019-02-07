7eabd3a9d6366677957f700749dd3bf0ebd1da34
Laurent <laurent@his.computer>
2019-02-07 20:36:39 +0100

    Fresh generation of Menu1 project.

---
+++ code/menu1/android/src/io/github/uborkaszoftver/menu1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.uborkaszoftver.menu1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.uborkaszoftver.menu1.Menu1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Menu1(), config);
+	}
+}
---
+++ code/menu1/desktop/src/io/github/uborkaszoftver/menu1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.uborkaszoftver.menu1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.uborkaszoftver.menu1.Menu1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Menu1(), config);
+	}
+}
