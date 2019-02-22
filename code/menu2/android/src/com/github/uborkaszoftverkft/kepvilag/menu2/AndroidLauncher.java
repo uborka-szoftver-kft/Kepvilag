f02d3d0b1378ae4afd39b0146b55aca8e80d9bcc
Laurent <laurent@his.computer>
2019-02-22 16:22:26 +0100

    Creation of 'menu2' project, using Kotlin. Issue 5574.

---
+++ code/menu2/android/src/com/github/uborkaszoftverkft/kepvilag/menu2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.uborkaszoftverkft.kepvilag.menu2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.uborkaszoftverkft.kepvilag.menu2.Menu2;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Menu2(), config);
+	}
+}
---
+++ code/menu2/desktop/src/com/github/uborkaszoftverkft/kepvilag/menu2/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.github.uborkaszoftverkft.kepvilag.menu2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.uborkaszoftverkft.kepvilag.menu2.Menu2;
+import com.github.uborkaszoftverkft.kepvilag.menu2.Menu2K;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Menu2K(), config);
+	}
+}
